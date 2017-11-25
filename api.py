44e9b0dc70c5b590246bc8842dabb3e97d413296
FranciscoJRSantos <franciscojrsantos1997@gmail.com>
2017-11-25 05:44:00 +0000

    Now showing challenges

--- api.py
+++ api.py
@@ -7,31 +7,41 @@ from firebase_admin import db
 from trello import TrelloApi
 from flask import request
 
+
 # Firebase auth
 cred = credentials.Certificate("./bi-tech-challenge-firebase-adminsdk-lb96n-c9e1358cc2.json")
 firebase_admin.initialize_app(cred, {'databaseURL' : 'https://bi-tech-challenge.firebaseio.com/'})
 root = db.reference()
 
+
 # Trello Auth
 trello = TrelloApi(os.environ.get('TRELLO_KEY'))
 trello.boards.get('4d5ea62fd76aa1136000000c')
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'safepasswordlol'
 auth = HTTPDigestAuth()
 
-@app.route("/")
+
+@app.route('/')
 def hello():
-    return "Hello World!"
+    return 'Hello World!'
+
 
 def get_auth(username,password):
-    # Query firebase || Leave this for now
-    users = root.get("users")
+    users = root.get('users')
     if username in users:
         return users.get(username)
     return None
 
-@app.route("/auth", methods=['GET','POST'])
+def get_challenges():
+    challenges = root.child('challenges').get()
+    print (challenges)
+    return challenges
+
+
+@app.route('/auth', methods=['GET','POST'])
 def auth():
     if request.method == 'POST':
         auth = get_auth(request.form['username'],request.form['password'])
@@ -40,8 +50,26 @@ def auth():
         return render_template('auth.html')
 
 
-@app.route("/plat",methods=['GET'])
+@app.route('/plat',methods=['GET'])
 def plaf():
     return render_template('login.html')
+
+
+@app.route('/challenge', methods=['GET'])
+def challenge():
+    challenges = get_challenges()
+    return render_template('challenge.html', challenges = challenges)
+
+
+@app.route('/challenge/add', methods=['GET','POST'])
+def challenge_add():
+    if request.method == 'POST':
+        challenge = {"name": request.form["name"], "description": request.form["description"], "location": request.form["location"], "prize": request.form["prize"]}
+        root.child("challenges").push(challenge)
+        return redirect('challenge')
+    else:
+        return render_template('add_challenge.html')
+
+
 if __name__ == '__main__':
     app.run()
