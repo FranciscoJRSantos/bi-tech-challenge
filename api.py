183f120e3ff344a93243dab0457ed3aa1ae1393a
FranciscoJRSantos <franciscojrsantos1997@gmail.com>
2017-11-26 00:13:20 +0000

    Changed everything

--- api.py
+++ api.py
@@ -1,10 +1,8 @@
 import os
 from flask import *
-from flask_httpauth import HTTPDigestAuth
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-from trello import TrelloApi
 from flask import request
 
 
@@ -13,25 +11,16 @@ cred = credentials.Certificate("./bi-tech-challenge-firebase-adminsdk-lb96n-c9e1
 firebase_admin.initialize_app(cred, {'databaseURL' : 'https://bi-tech-challenge.firebaseio.com/'})
 root = db.reference()
 
-
-# Trello Auth
-trello = TrelloApi(os.environ.get('TRELLO_KEY'))
-trello.boards.get('4d5ea62fd76aa1136000000c')
-
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'safepasswordlol'
-auth = HTTPDigestAuth()
-
-@app.route('/')
-def hello():
-    return 'Hello World!'
 
 
-def get_auth(username):
-    users = root.get('users')
-    if username in users:
-        return users.get(username)
+def get_auth(username, password):
+    print (username)
+    users = root.child('users').get()
+    for (key,value) in users.items():
+        if value["email"] == username and value["password"] == password:
+            return value
     return None
 
 def get_challenges():
@@ -41,18 +30,6 @@ def get_challenges():
 def get_users():
     users = root.child('users').get()
     return users
-
-def get_sections():
-    sections = root.child('sections').get()
-    return sections
-
-def get_teams():
-    teams = root.child('teams').get()
-    return teams
-
-def get_countries():
-    countries = root.child('countries').get()
-    return countries
 
 def get_username(username):
     users = root.child('users').get()
@@ -68,31 +45,53 @@ def get_usermail(mail):
             return key,value 
     return False
 
+def get_trains():
+    trains = root.child('trains').get()
+    return trains
+
+def get_forum_questions():
+    forum_questions = root.child('forum').get()
+    return forum_questions
+
+def get_question(question):
+    forum_questions = get_forum_questions()
+    for (key,value) in forum_questions.items():
+        if value["title"] == question:
+            return value
+    return false
+
+#def answer_question(author, answer):
+#    forum_questions = get_forum_questions()
+#    for (key,question) in forum_questions.items():
+#        for (key,answer) in question["answers"]:
+#            if question["answers"][]
+
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
-        auth = get_auth(request.form['username'])
-        return render_template('home.html') if auth else redirect('plat')
+        auth = get_auth(request.form['username'], request.form['password'])
+        if auth:
+            return redirect('platform')
+        else:
+            return redirect('login')
     else:
         return render_template('auth.html')
 
 
-@app.route('/plat',methods=['GET'])
-def plat():
+@app.route('/platform',methods=['GET'])
+def platform():
     users = get_users()
-    users_list = sorted(users.items(), key=lambda x: x[1]["points"], reverse = True)
-    teams = get_teams()
-    teams_list = sorted(teams.items(), key=lambda x: x[1]["points"], reverse = True)
-    sections = get_sections()
-    sections_list = sorted(sections.items(), key=lambda x: x[1]["points"], reverse = True)
-    countries = get_countries()
-    countries_list = sorted(countries.items(), key=lambda x: x[1]["points"], reverse = True)
-    return render_template('login.html', users_list = users_list, teams_list = teams_list, sections_list = sections_list, countries_list = countries_list)
+    wp_list = sorted(users.items(), key=lambda x: x[1]["wp"], reverse = True)
+    fp_list = sorted(users.items(), key=lambda x: x[1]["fp"], reverse = True)
+    hp_list = sorted(users.items(), key=lambda x: x[1]["hp"], reverse = True)
+    return render_template('platform.html', wp_list = wp_list, fp_list = fp_list, hp_list = hp_list)
 
 
 @app.route('/challenge', methods=['GET'])
 def challenge():
     challenges = get_challenges()
+    if challenges is None:
+        challenges = {}
     return render_template('challenge.html', challenges = challenges)
 
 
@@ -105,25 +104,41 @@ def challenge_add():
     else:
         return render_template('add_challenge.html')
 
-@app.route('/profile/<username>', methods=['GET','POST'])
+@app.route('/profile/<username>', methods=['GET'])
 def profile(username):
     user = get_username(username)
-    if request.method == 'POST':
-        give_user_points(request.form["mail"],request.form["points"])
-        return redirect("plat")
+    if user:
+        return render_template('profile.html', user = user)
     else:
-        if user:
-            return render_template('profile.html', user = user)
-        else:
-            return abort(404)
+        return abort(404)
 
-def give_user_points(email,points):
-    (user_key,user) = get_usermail(email)
-    print(user_key, user)
-    if user and user["email"] != mail:
-        user["points"] = user["points"] + points
-        root.child("user").child(user_key).update({"points": user["points"]})
-    return redirect('plat')
+@app.route('/train', methods = ['GET'])
+def train():
+    trains = get_trains()
+    return render_template('train.html',trains = trains)
+
+@app.route('/forum', methods = ['GET'])
+def forum():
+    forum = get_forum_questions()
+    return render_template('forum.html', forum = forum)
+
+@app.route('/forum/add', methods = ['GET', 'POST'])
+def create_question():
+    if request.method == 'POST':
+        # Add stuff
+        print("oi")
+    else:
+        return render_template('add_question.html')
+
+@app.route("/forum/show/<question>", methods=['GET','POST'])
+def show_question(question):
+    if request.method == 'POST':
+        answer_question("Xico Santos", request.form["answer"])
+        render_template('show_question.html', question = question)
+    else:
+        question = get_question(question)
+    return render_template('show_question.html', question = question) 
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
